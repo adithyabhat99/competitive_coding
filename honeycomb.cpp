57a9e15e46bb8c619ec3f6613c693a10a1bafead
adithyabhat99 <adithyabhatoct@gmail.com>
2018-11-25 22:22:25 +0530

    added new program

--- honeycomb.cpp
+++ honeycomb.cpp
@@ -1,3 +1,19 @@
+/*You are bee stuck in a cell in a honeycomb. A honeycomb is a grid of hexagonal cells each having six neighbouring hexagonal cells.
+The exit of the honeycomb is located at cell number 1. (refer honeycomb.jpg image)
+You are given the pattern of the grid below. You need to find the minimum number of cells you have to cross to reach cell 1.
+Note that the image labels the cells from 1 to 25. This pattern will be continued.
+
+Input Format
+
+The first line contains a single integer n which is the total number of queries.
+n lines follow. Each of these lines contain a single integer x indicating the current cell number
+Constraints
+
+Output Format
+
+For each of the n inputs print a single line with a single integer d which is the minumum distance from cell number x to 1.
+Distance is the count of cells that were traversed before reaching the destination.
+*/
 #include <cmath>
 #include <cstdio>
 #include <vector>
