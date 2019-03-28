c943b59c4f33dbe6d77b7e728d14e79f17478f43
adithyabhat99 <adithyabhatoct@gmail.com>
2019-03-28 22:52:29 +0530

    added new files

--- matrixMinCost.cpp
+++ matrixMinCost.cpp
@@ -1,30 +1,3 @@
-//Recursion:
-#include<iostream>
-using namespace std;
-int fun(int **a,int m,int n,int i,int j){
-if(i==m-1 && j==n-1)
-return a[m-1][n-1];
-if(i<0 || j<0 || i>=m || j>=n)
-return 9999;
-return a[i][j]+min(fun(a,m,n,i+1,j),fun(a,m,n,i,j+1));
-}
-int main(){
-int i,j,m,n;
-cin>>m>>n;
-int **a;
-a=new int*[m];
-for(i=0;i<m;i++)
-a[i]=new int[n];
-for(i=0;i<m;i++)
-for(j=0;j<n;j++)
-cin>>a[i][j];
-cout<<fun(a,m,n,0,0)<<endl;
-return 0;
-}
-
-
-
-//DP:
 #include<iostream>
 using namespace std;
 int fun(int **a,int m,int n){
