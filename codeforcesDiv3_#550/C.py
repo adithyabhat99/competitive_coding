815d9df499cd562bbed4c89b56c3e2df4871655f
adithyabhat99 <adithyabhatoct@gmail.com>
2019-04-02 01:11:48 +0530

    new files

--- codeforcesDiv3_#550/C.py
+++ codeforcesDiv3_#550/C.py
@@ -1,42 +1,23 @@
-#Wrong answer !
-
 from collections import Counter
-n=int(input())
-l=input().split()
-count=Counter(l)
-c=1
-for i in count.values():
-    if int(i)>2:
+n = int(input())
+a = sorted(list(map(int, input().split())))
+for s in list(Counter(a).values()):
+    if s > 2:
         print("NO")
-        c=0
-        break
-if c==1:
-    x=0
-    inc=[]
-    dec=[]
-    print("YES")
-    l.sort()
-    while x<len(l) and x+1<len(l):
-        if l[x]!=l[x+1]:
-            inc.append(l[x])
-            x+=1
-        else:
-            inc.append(l[x])
-            dec.append(l[x+1])
-            x+=2
-    print(len(inc))
-    inc=[int(i) for i in inc]
-    for i in inc:
-        print(i,end=" ")
-    print(len(dec))
-    dec = [int(i) for i in dec][::-1]
-    dec.reverse()
-    for i in dec:
-        print(i,end=" ")
-    print("")
-    
-    
-    
-
-
-
+        exit()
+inc = []
+dec = []
+t = 0
+for char in a:
+    if t == 0:
+        inc.append(char)
+    else:
+        dec.append(char)
+    t = (t+1) % 2
+inc = [str(i) for i in inc]
+dec = [str(i) for i in dec]
+print("YES")
+print(len(inc))
+print(' '.join(inc))
+print(len(dec))
+print(' '.join(dec[::-1]))
